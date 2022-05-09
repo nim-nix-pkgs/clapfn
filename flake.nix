1c5260cfaf5056deef479b909b08afdf2413b465
hugosenari <hugosenari@gmail.com>
2022-05-09 20:36:03 -0300

    chore: re index clapfn

--- flake.nix
+++ flake.nix
@@ -7,14 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."clapfn-master".dir   = "master";
-  inputs."clapfn-master".owner = "nim-nix-pkgs";
-  inputs."clapfn-master".ref   = "master";
-  inputs."clapfn-master".repo  = "clapfn";
-  inputs."clapfn-master".type  = "github";
-  inputs."clapfn-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."clapfn-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
